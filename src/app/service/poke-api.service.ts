a6baea2c94de0a334bfc560478eeec6cac4f8c56
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-06-25 19:31:07 -0300

    consumindo os dados da api

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +11,18 @@ export class PokeApiService {
   constructor(private http: HttpClient) {}
 
   get apiListAllPokemons(): Observable<any> {
-    return this.http.get<any>(this.url);
+    return this.http.get<any>(this.url).pipe(
+      tap((res) => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            (res) => (resPokemons.status = res)
+          );
+        });
+      })
+    );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
