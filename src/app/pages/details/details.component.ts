a900aaa01cdb91414685b637608239b8fc31f451
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-06-25 21:16:07 -0300

    desenvolvendo a funcao get Pokemon no details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon() {
+    const id = this.activateRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
